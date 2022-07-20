851c30afe5522fb5feaebee73916ab3f41e1f8a3
SROP6313 <103128273+SROP6313@users.noreply.github.com>
2022-07-21 01:01:19 +0800

    Add files via upload

--- movieword_2.py
+++ movieword_2.py
@@ -1,42 +1,137 @@
-from cProfile import label
-from enum import auto
-import openpyxl
+from tkinter import *
+from tkinter import ttk
 import tkinter as tk
-
-from pyparsing import col
+import openpyxl
+from openpyxl import load_workbook
 
 win = tk.Tk()
-win.geometry("800x300")
+win.geometry("950x300")
 win.title("熊的單字庫")
 
 Ewordafter = tk.StringVar()
 Cwordafter = tk.StringVar()
 FromName = tk.StringVar()
 Ewordbefore = tk.StringVar()
+WorksheetName = tk.StringVar()
+addEword = tk.StringVar()
+addCword = tk.StringVar()
+warnword = tk.StringVar()
+SheetName = tk.StringVar()
 workbook = openpyxl.load_workbook('moviewo.xlsx')
 count=len(workbook.sheetnames)
 
 def searchEword():
+    workbook = openpyxl.load_workbook('moviewo.xlsx')
+    count=len(workbook.sheetnames)
     EwordafterString = ""
     CwordafterString = ""
     FromNameString = ""
+    wordafternum = 0
     for k in range(0,count):
             sheet = workbook.worksheets[k]
             for i in range(1,sheet.max_row+1):
                 for j in range(1,sheet.max_column+1):
                     if Ewordbefore.get() == str(sheet.cell(row=i,column=j).value):
-                        EwordafterString = EwordafterString + "\n" + str(sheet.cell(row=i,column=j).value)
-                        CwordafterString = CwordafterString + "\n" + str(sheet.cell(row=i,column=j+1).value)
-                        FromNameString = FromNameString + "\n" + str(workbook.sheetnames[k])
+                        wordafternum = wordafternum + 1
+                        if wordafternum == 1:
+                            EwordafterString = str(sheet.cell(row=i,column=j).value)
+                            CwordafterString = str(sheet.cell(row=i,column=j+1).value)
+                            FromNameString = str(workbook.sheetnames[k])
+                        else:
+                            EwordafterString = EwordafterString + "\n" + str(sheet.cell(row=i,column=j).value)
+                            CwordafterString = CwordafterString + "\n" + str(sheet.cell(row=i,column=j+1).value)
+                            FromNameString = FromNameString + "\n" + str(workbook.sheetnames[k])
                         break
+    
     Ewordafter.set(EwordafterString)
     Cwordafter.set(CwordafterString)
     FromName.set(FromNameString)
-    EwordafterString = ""
-    CwordafterString = ""
-    FromNameString = ""
+    if EwordafterString == "":
+        Ewordafter.set("查無此單字")
 
-frame1 = tk.Frame(win)
+def addword():
+    WorksheetNameString = WorksheetName.get()
+    addEwordString = addEword.get()
+    addCwordString = addCword.get()
+    sheetneedCreate = 1
+    entryerror = 0
+    wb = load_workbook('moviewo.xlsx')
+    count=len(wb.sheetnames)
+
+    if (WorksheetNameString == "") or (addEwordString == "") or (addCwordString == ""):
+        entryerror = 1
+        label8.config(fg="red")
+        warnword.set("輸入不完整!請重新輸入")
+
+    if entryerror == 0:
+        for i in range(0,count):
+            if WorksheetNameString == wb.sheetnames[i]:
+                sheetneedCreate = 0
+                sheet = wb.worksheets[i]
+                dataWrite = [addEwordString,addCwordString]
+                sheet.append(dataWrite)
+                label8.config(fg="blue")
+                warnword.set("工作表已存在，單字儲存成功!")
+
+        if sheetneedCreate == 1:
+            ws1 = wb.create_sheet(WorksheetNameString, count)
+            sheet = wb.worksheets[count]
+            dataWrite = [addEwordString,addCwordString]
+            sheet.append(dataWrite)
+            label8.config(fg="blue")
+            warnword.set("已創建新的工作表，單字儲存成功!")
+        
+        count=len(wb.sheetnames)
+        wb.save('moviewo.xlsx')
+        wb.close()
+
+def renewsheetname():
+    SheetNameString =""
+    workbook = openpyxl.load_workbook('moviewo.xlsx')
+    count=len(workbook.sheetnames)
+    for i in range(0, count):
+        SheetNameString = SheetNameString + "," + workbook.sheetnames[i]
+    
+    SheetName.set(SheetNameString)
+
+
+tabsystem = ttk.Notebook(win)
+tab1 = Frame(tabsystem)
+tab2 = Frame(tabsystem)
+tab3 = Frame(tabsystem)
+
+tabsystem.add(tab1, text='新增單字')
+tabsystem.add(tab2, text='查詢單字')
+tabsystem.add(tab3, text='工作表名稱庫')
+tabsystem.pack(expand=1, fill="both")
+
+#-------------------------分頁 1---------------------------
+
+labelNone4 = tk.Label(tab1, text="")
+label5 = tk.Label(tab1, text="工作表名稱：", font=("微軟正黑體", 18))
+entry2 = tk.Entry(tab1, textvariable=WorksheetName, font=("微軟正黑體", 18))
+label6 = tk.Label(tab1, text="增加的英文單字：", font=("微軟正黑體", 18))
+entry3 = tk.Entry(tab1, textvariable=addEword, font=("微軟正黑體", 18))
+label7 = tk.Label(tab1, text="中文意思：", font=("微軟正黑體", 18))
+entry4 = tk.Entry(tab1, textvariable=addCword, font=("微軟正黑體", 18))
+labelNone3 = tk.Label(tab1, text="")
+button2 = tk.Button(tab1, text="增加至單字庫", font=("微軟正黑體", 18), fg="blue", command=addword)
+label8 = tk.Label(tab1, textvariable=warnword, font=("微軟正黑體", 14))
+
+labelNone4.grid(row=0, column=0)
+label5.grid(row=1, column=0, sticky="e")
+entry2.grid(row=1, column=1)
+label6.grid(row=2, column=0, sticky="e")
+entry3.grid(row=2, column=1)
+label7.grid(row=3, column=0, sticky="e")
+entry4.grid(row=3, column=1)
+labelNone3.grid(row=4, column=0)
+button2.grid(row=5, column=1, sticky="w")
+label8.place(x=190, y=210)
+
+#-------------------------分頁 2---------------------------
+
+frame1 = tk.Frame(tab2)
 frame1.pack()
 label1 = tk.Label(frame1, text="想查詢的單字：", font=("微軟正黑體", 18))
 entry = tk.Entry(frame1, textvariable=Ewordbefore, font=("微軟正黑體", 18))
@@ -47,13 +142,13 @@ entry.grid(row=0, column=1)
 labelNone1.grid(row=0, column=2)
 button1.grid(row=0, column=3)
 
-labelNone2 = tk.Label(win, text="")
+labelNone2 = tk.Label(tab2, text="")
 labelNone2.pack()
 
-frame2 = tk.Frame(win)
+frame2 = tk.Frame(tab2)
 frame2.pack()
-labeltitle1 = tk.Label(frame2, text="英文單字".ljust(15), font=("微軟正黑體", 18), bg="black", fg="white")
-labeltitle2 = tk.Label(frame2, text="中文".ljust(60), font=("微軟正黑體", 18), bg="black", fg="white")
+labeltitle1 = tk.Label(frame2, text="英文單字".ljust(18), font=("微軟正黑體", 18), bg="black", fg="white")
+labeltitle2 = tk.Label(frame2, text="中文".ljust(80), font=("微軟正黑體", 18), bg="black", fg="white")
 labeltitle3 = tk.Label(frame2, text="出處".ljust(30), font=("微軟正黑體", 18), bg="black", fg="white")
 label2 = tk.Label(frame2, textvariable=Ewordafter, font=("微軟正黑體", 14))
 label3 = tk.Label(frame2, textvariable=Cwordafter, font=("微軟正黑體",14))
@@ -64,5 +159,12 @@ labeltitle3.grid(row=0, column=2)
 label2.grid(row=1, column=0, sticky="w")
 label3.grid(row=1, column=1, sticky="w")
 label4.grid(row=1, column=2, sticky="w")
+
+#-------------------------分頁 3---------------------------
+
+button3 = tk.Button(tab3, text="更新", font=("微軟正黑體", 18), command=renewsheetname)
+label9 = tk.Label(tab3, textvariable=SheetName, font=("微軟正黑體", 12))
+button3.pack()
+label9.pack()
 
 win.mainloop()
